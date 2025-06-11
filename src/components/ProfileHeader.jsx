85f45eb2f34ed9d6eeeff0eef8771e5e7caaee8d
kaiser-shah <shah.docklands@gmail.com>
2025-06-11 14:12:03 +0800

    Initial Commit

---
+++ src/components/ProfileHeader.jsx
@@ -0,0 +1,59 @@
+import { useContext } from "react"
+import { Col, Row, Image, Button } from "react-bootstrap"
+import { ProfileContext } from "../App"
+
+export default function ProfileHeader() {
+    const {
+        image,
+        name,
+        posts_no,
+        followers,
+        following,
+        subheader,
+        account_type,
+        descripton,
+        link } = useContext(ProfileContext)
+
+
+    return (
+        <Row className="p-5">
+            <Col md={3} className="d-flex align-items-center justify-content-center">
+                <Image
+                    src={"https://sig1.co/logo-1"}
+                    style={{ height: "150px" }}
+                    roundedCircle
+                />
+            </Col>
+            <Col md={9}>
+                <span className="me-4" style={{ fontSize: "20px" }}>
+                    {name}
+                </span>
+                <Button className="me-2">Follow Back</Button>
+                <Button variant="light me-2">Message</Button>
+                <Button variant="light me-2">
+                    <i className="bi bi-person-plus"></i>
+                </Button>
+                <Button variant="light">
+                    <i className="bi bi-three-dots"></i>
+                </Button>
+                <br />
+                <br />
+                <span className="me-4">
+                    <strong>{posts_no}</strong> post
+                </span>
+                <span className="me-4">
+                    <strong>{followers}</strong> followers
+                </span>
+                <span className="me-4">
+                    <strong>{following}</strong> following
+                </span>
+                <br />
+                <br />
+                <p style={{ margin: 0, fontWeight: "bold" }}>{subheader}</p>
+                <p style={{ margin: 0, color: "grey" }}>{account_type}</p>
+                <p style={{ margin: 0 }}>{descripton}</p>
+                <a href={link}>{link}</a>
+            </Col>
+        </Row>
+    )
+}
